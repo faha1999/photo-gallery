9ecadb625479d0e54e86fff96c460075253a61b3
faha1999 <kafaha1999@gmail.com>
2022-04-17 19:48:07 +0600

    Firebase storage hook added

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,29 @@
+import { async } from '@firebase/util';
+import { useState, useEffect } from 'react';
+import { projectStorage } from '../fairbase/config';
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    const storageRef = projectStorage.ref(file.name);
+
+    storageRef.put(file).on('sate_changed', (snap) => {
+      let percentage = (snap.bytesTransfer / snap.totalBytes) * 100;
+      setProgress(percentage);
+    }),
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        setUrl(url);
+      };
+  }, [file]);
+
+  return { progress, url, error };
+};
+
+export default useStorage;
