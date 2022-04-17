22bdfa9c9cb5dd1d9ca1667129ec44e226b298a1
faha1999 <kafaha1999@gmail.com>
2022-04-17 18:15:55 +0600

    Uploaded form and condition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/style.min.css';
 import Title from './components/Title';
+import UploadFrom from './components/UploadFrom';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <UploadFrom />
     </div>
   );
 }
